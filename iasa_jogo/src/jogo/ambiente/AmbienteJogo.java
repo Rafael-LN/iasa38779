1ef9d50af606422bb2185f642ab184ebcf4b3638
Rafael-LN <rafaelnunes3092@gmail.com>
2024-03-11 18:51:47 +0000

    Add comments to AmbienteJogo class

--- iasa_jogo/src/jogo/ambiente/AmbienteJogo.java
+++ iasa_jogo/src/jogo/ambiente/AmbienteJogo.java
@@ -8,43 +8,63 @@ import ambiente.Ambiente;
 import ambiente.Comando;
 import ambiente.Evento;
 
+/**
+ * Classe que representa o ambiente do jogo.
+ * Esta classe controla a evolução do ambiente do jogo, gera eventos e permite que a personagem
+ * observe e execute comandos no ambiente.
+ * Implementa a interface Ambiente para definir o comportamento do ambiente.
+ */
 public class AmbienteJogo implements Ambiente{
 
     private EventoJogo evento;
     private List<EventoJogo> eventos;
     
+    /**
+     * Inicializa a lista de eventos com um evento inicial.
+     */
     public AmbienteJogo(){
         eventos = new ArrayList<EventoJogo>();
         eventos.add(EventoJogo.SILENCIO);
     }
 
+    /**
+     * Evolui o ambiente do jogo, gerando um novo evento.
+     */
     @Override
     public void evoluir() {
         eventos.add(gerarEvento());
     }
 
+     /**
+     * Retorna o evento atual no ambiente.
+     */
     @Override
     public Evento observar() {
         return evento;
     }
 
+    /**
+     * Executa um comando no ambiente do jogo.
+     */
     @Override
     public void executar(Comando comando) {
         comando.mostrar();
     }
 
+    /**
+     * Gera um novo Evento com base no utilizador
+     */
     private EventoJogo gerarEvento(){
         try (Scanner scanner = new Scanner(System.in)) {
             String s = scanner.nextLine();
-
-            System.out.println(s);
-
-            System.out.println(EventoJogo.valueOf(s.toUpperCase()));
 
             return EventoJogo.valueOf(s.toUpperCase());
         }
     }
 
+    /**
+     * Obtém o evento atual no ambiente do jogo.
+     */
     public EventoJogo getEvento(){
         return evento;
     }
