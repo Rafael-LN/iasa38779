6dfe4aa33ad4ed3f3ae25e081fafb57aa551c7dd
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-08 21:11:09 +0100

    Add constructor to AproximarDir

--- iasa_agente/src/lib/controlo_react/reaccoes/aproximar/aproximarDir.py
+++ iasa_agente/src/lib/controlo_react/reaccoes/aproximar/aproximarDir.py
@@ -1,3 +1,5 @@
+from controlo_react.reaccoes.aproximar.aproximarAlvo import AproximarAlvo
+from controlo_react.reaccoes.resposta.respostaMover import RespostaMover
 from ecr.reaccao import Reaccao
 
 
@@ -6,3 +8,5 @@ class AproximarDir(Reaccao):
     Classe que representa uma reação para aproximar o agente numa direção específica.
     Herda da classe Reaccao.
     """
+    def __init__(self, direccao):
+        super(AproximarAlvo(direccao), RespostaMover(direccao))
