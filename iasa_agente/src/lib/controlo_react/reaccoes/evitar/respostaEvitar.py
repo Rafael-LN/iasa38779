2bf706bb6d5f70063833462e178d04e262d5f7f3
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-08 22:36:07 +0100

    Add respostaEvitar class

---
+++ iasa_agente/src/lib/controlo_react/reaccoes/evitar/respostaEvitar.py
@@ -0,0 +1,41 @@
+from random import choice
+from controlo_react.reaccoes.resposta.respostaMover import RespostaMover
+from sae.agente.accao import Accao
+from sae.ambiente.direccao import Direccao
+
+
+class RespostaEvitar(RespostaMover):
+    
+    def __init__(self, dir_inicial = Direccao.ESTE):
+        super().__init__(dir_inicial)
+    
+    def activar(self, percepcao, intensidade):
+        if percepcao.contacto_obst(self.dir_inicial):
+            self._accao.direccao = self.dir_inical
+        else:
+            self._accao.direccao = self.__alterar_direccao(percepcao)
+        
+        self._accao.prioridade = intensidade
+        
+        return self._accao
+    
+    
+    def __alterar_direccao(self, percepcao):
+        """
+        Alterar direção do agente
+        @param percepcao: percepção actual
+        @return: direcao livre (sim/nao)
+        """
+        direccao_livre = self.__direccao_livre(percepcao)
+        
+        if direccao_livre:
+            self._accao.direccao = direccao_livre
+        
+        return direccao_livre
+    
+    def __direccao_livre(self, percepcao):
+        while True:
+            direccao = choice([direccao for direccao in Direccao])
+            if percepcao.contacto_obst(direccao):
+                return direccao
+        
