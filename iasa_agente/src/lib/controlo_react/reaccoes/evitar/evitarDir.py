74a0e1d5918ed389edd634f38404737cae93498a
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-08 19:46:36 +0100

    Add comments

--- iasa_agente/src/lib/controlo_react/reaccoes/evitar/evitarDir.py
+++ iasa_agente/src/lib/controlo_react/reaccoes/evitar/evitarDir.py
@@ -2,3 +2,7 @@ from ecr.reaccao import Reaccao
 
 
 class EvitarDir(Reaccao):
+    """
+    Classe que representa uma reação para evitar uma direção específica.
+    Herda da classe Reaccao.
+    """
