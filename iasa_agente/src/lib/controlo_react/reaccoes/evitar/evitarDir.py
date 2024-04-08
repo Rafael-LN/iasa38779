186ba439005430a6c0fd23bd24b6a7866225c38d
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-08 21:44:17 +0100

    Add contructor to EvitarDir

--- iasa_agente/src/lib/controlo_react/reaccoes/evitar/evitarDir.py
+++ iasa_agente/src/lib/controlo_react/reaccoes/evitar/evitarDir.py
@@ -1,3 +1,4 @@
+from controlo_react.reaccoes.evitar.estimuloObst import EstimuloObst
 from ecr.reaccao import Reaccao
 
 
@@ -6,3 +7,5 @@ class EvitarDir(Reaccao):
     Classe que representa uma reação para evitar uma direção específica.
     Herda da classe Reaccao.
     """
+    def __init__(self, direccao, resposta):
+        super().__init__(EstimuloObst(direccao), resposta)
