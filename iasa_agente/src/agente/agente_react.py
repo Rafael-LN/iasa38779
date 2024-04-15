c08227ba03a2f677d2303153814ca10e0990c18e
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-15 18:55:45 +0100

    Refactor constructor of AgenteReact class

--- iasa_agente/src/agente/agente_react.py
+++ iasa_agente/src/agente/agente_react.py
@@ -1,4 +1,6 @@
 from controlo_react.controloReact import ControloReact
+from controlo_react.reaccoes.aproximar.aproximarAlvo import AproximarAlvo
+from controlo_react.reaccoes.evitar.evitarObst import EvitarObst
 from controlo_react.reaccoes.explorar.explorar import Explorar
 from controlo_react.reaccoes.recolher import Recolher
 from sae.agente.agente import Agente
@@ -8,7 +10,8 @@ from sae.simulador import Simulador
 class AgenteReact(Agente):
     
     def __init__(self):
-        controlo = ControloReact(Recolher([Explorar()]))
+        controlo = ControloReact(Recolher([Explorar(), AproximarAlvo(), EvitarObst()]))
+        controlo.mostrar_per_dir = True
         super().__init__(controlo)
         
 if __name__ == "__main__":
