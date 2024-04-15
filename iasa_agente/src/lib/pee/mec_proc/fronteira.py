bd788690408e7848e950ad51a623bf539dd45f66
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-15 22:18:34 +0100

    Add property dimensao to Fronteira class

--- iasa_agente/src/lib/pee/mec_proc/fronteira.py
+++ iasa_agente/src/lib/pee/mec_proc/fronteira.py
@@ -10,6 +10,12 @@ class Fronteira(ABC):
     def vazia(self):
         return len(self._nos) == 0
 
+    @property
+    def dimensao(self):
+        """
+        Retorna a dimensão da estrutura de nós (fronteira)
+        """
+        return len(self._nos)
     
     def iniciar(self): pass
     
