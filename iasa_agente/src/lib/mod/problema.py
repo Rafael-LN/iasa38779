bf1fe0417a54400e213ffbcc9fc0fb7e0ca345cc
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-15 22:43:23 +0100

    Add functional comments to Problema class

--- iasa_agente/src/lib/mod/problema.py
+++ iasa_agente/src/lib/mod/problema.py
@@ -1,11 +1,37 @@
 from abc import ABC, abstractmethod
 
-
 class Problema(ABC):
-    
+    """
+    Classe abstrata que define um problema a ser resolvido.
+
+    Attributes:
+        estado_inicial: O estado inicial do problema.
+        operadores: Uma lista de operadores disponíveis para resolver o problema.
+
+    Métodos Abstratos:
+        objectivo(estado): Método abstrato que verifica se um determinado estado atinge o objetivo do problema.
+    """
+
     def __init__(self, estado_inicial, operadores):
+        """
+        Inicializa o problema com o estado inicial e a lista de operadores disponíveis.
+
+        Args:
+            estado_inicial: O estado inicial do problema.
+            operadores: Uma lista de operadores disponíveis para resolver o problema.
+        """
         self.estado_inicial = estado_inicial
         self.operadores = operadores
         
     @abstractmethod
-    def objectivo(self, estado): pass
+    def objectivo(self, estado):
+        """
+        Verifica se um determinado estado atinge o objetivo do problema.
+
+        Args:
+            estado: O estado a ser verificado.
+
+        Returns:
+            True se o estado atinge o objetivo do problema, False caso contrário.
+        """
+        pass
