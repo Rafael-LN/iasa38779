d26f8084576d1f73878afb5cd11163ed7a56fffe
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-15 22:41:22 +0100

    Add functional comments to PassoSolucao dataclass

--- iasa_agente/src/lib/pee/mec_proc/passoSolucao.py
+++ iasa_agente/src/lib/pee/mec_proc/passoSolucao.py
@@ -6,5 +6,12 @@ from mod.operador import Operador
 
 @dataclass
 class PassoSolucao():
+    """
+    Classe que representa um passo de uma solução, contendo um estado e o operador que levou a esse estado.
+
+    Attributes:
+        estado (Estado): O estado associado a este passo da solução.
+        operador (Operador): O operador que foi aplicado para chegar a este estado.
+    """
     estado: Estado
     operador: Operador
