1b2e6fd78fb8220c72ff9ce037c078bb77b58571
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-15 18:52:59 +0100

    Add functional comments to EstimuloAlvo

--- iasa_agente/src/lib/controlo_react/reaccoes/aproximar/estimuloAlvo.py
+++ iasa_agente/src/lib/controlo_react/reaccoes/aproximar/estimuloAlvo.py
@@ -1,15 +1,32 @@
 from ecr.estimulo import Estimulo
 
-
 class EstimuloAlvo(Estimulo):
-    
-    def __init__(self, direccao, gama = 0.9):
-        self.__direccao = direccao
-        self.__gama = gama
+    """
+    Classe que representa um estímulo associado a um alvo em uma direção específica.
+    Herda da classe Estimulo.
+    """
+
+    def __init__(self, direccao, gama=0.9):
+        """
+        Método construtor da classe EstimuloAlvo.
         
+        Args:
+            direccao (int): Direção do alvo.
+            gama (float, opcional): Fator de desconto para o valor do estímulo (padrão é 0.9).
+        """
+        self.__direccao = direccao  # Direção do alvo
+        self.__gama = gama  # Fator de desconto para o valor do estímulo
+
     def detectar(self, percepcao):
-        perDir = percepcao[self.__direccao]
-        if(perDir.elemento == "A"):
-            return perDir.distancia ** self.__gama
-        return 0
+        """
+        Método para detectar a presença do alvo na percepção.
         
+        Args:
+            percepcao (list): Percepção atual do ambiente.
+            
+        Returns:
+            O valor do estímulo associado à presença do alvo na direção especificada.
+        """
+        if percepcao[self.__direccao] == "A":  # Se o alvo estiver presente na direção especificada
+            return percepcao[1] ** self.__gama  # Retorna o valor do estímulo com desconto gamma
+        return 0  # Retorna 0 se o alvo não estiver presente na direção especificada
