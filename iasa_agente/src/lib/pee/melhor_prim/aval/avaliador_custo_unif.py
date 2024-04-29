f73880c8b0599a78fb108ea38d5202a067ccb59f
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-29 22:38:09 +0100

    Add prioridade method to AvaliadorCustoUnif

--- iasa_agente/src/lib/pee/melhor_prim/aval/avaliador_custo_unif.py
+++ iasa_agente/src/lib/pee/melhor_prim/aval/avaliador_custo_unif.py
@@ -3,4 +3,4 @@ from pee.melhor_prim.aval.avaliador import Avaliador
 
 class AvaliadorCustoUnif(Avaliador):
     def prioridade(self, no):
-        return super().prioridade(no)
+        return no.custo
