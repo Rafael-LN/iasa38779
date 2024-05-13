d0658c3f9b203fc214dfd8435e58b4276af52c8c
Rafael-LN <rafaelnunes3092@gmail.com>
2024-05-13 20:48:20 +0100

    Add PlaneadorPEE class

---
+++ iasa_agente/src/lib/plan/plan_pee/planeador_pee.py
@@ -0,0 +1,9 @@
+from plan.planeador import Planeador
+
+
+class PlaneadorPEE(Planeador):
+    def __init__(self):
+        self.__mec_pee
+        
+    def planear(self, modelo_plan, objectivos): pass
+    
