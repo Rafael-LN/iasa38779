300afc21e2eebd255427bbc717a56a5a5527b976
Rafael-LN <rafaelnunes3092@gmail.com>
2024-05-13 21:35:47 +0100

    Implement plano_pee class

--- iasa_agente/src/lib/plan/plan_pee/plano_pee.py
+++ iasa_agente/src/lib/plan/plan_pee/plano_pee.py
@@ -1,9 +1,25 @@
 class PlanoPEE():
     def __init__(self, solucao):
-        self.__solucao = solucao
+        self.__passos = [passo for passo in solucao]
         
     def obter_accao(self, estado):
-        pass
+        """
+        Se houver passos
+            Retirar o passo de passos
+            Se passo for igual a estado
+            Retorna o operador do passo
+        """
+        if self.__passos:
+            passo = self.__passos.pop()
+            if passo.estado == estado:
+                return passo.operador
     
     def mostrar(self, vista):
-        pass
+        """
+        Mostrar 
+        
+        """
+        if self.__passos:
+            # Mostrar passos do plano como vectores
+            for passo in self.__passos:
+                vista.mostrar_vector(passo.estado.posicao, passo.operador.ang)
