4b5f73b9251462ecacb620fc0328bf55749ba496
Rafael-LN <rafaelnunes3092@gmail.com>
2024-05-13 20:50:59 +0100

    Add plano_pee class

---
+++ iasa_agente/src/lib/plan/plan_pee/plano_pee.py
@@ -0,0 +1,9 @@
+class PlanoPEE():
+    def __init__(self, solucao):
+        self.__solucao = solucao
+        
+    def obter_accao(self, estado):
+        pass
+    
+    def mostrar(self, vista):
+        pass
