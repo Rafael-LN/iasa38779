04b0dcca974ee1f74e9332417ff424f87a13beaa
Rafael-LN <rafaelnunes3092@gmail.com>
2024-05-13 21:59:15 +0100

    Rename problema plan and implement method

--- iasa_agente/src/lib/plan/plan_pee/mod_prob/problema_plan.py
+++ iasa_agente/src/lib/plan/plan_pee/mod_prob/problema_plan.py
@@ -1,9 +1,10 @@
 from mod.problema import Problema
 
 
-class ProblemaPan(Problema):
+class ProblemaPlan(Problema):
     def __init__(self, modelo_plan, estado_final):
+        super().__init__(modelo_plan.obter_estados().pop(0), modelo_plan.obter_operadores())
         self.__estado_final = estado_final
         
     def objectivo(self, estado):
-        return super().objectivo(estado)
+        return estado == self.__estado_final
