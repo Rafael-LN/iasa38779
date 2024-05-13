cc6fea01b97d34b7098227e11c851bbde3f22f88
Rafael-LN <rafaelnunes3092@gmail.com>
2024-05-13 21:46:31 +0100

    Add constructor and method from interface

--- iasa_agente/src/lib/plan/plan_pee/mod_prob/problema_pan.py
+++ iasa_agente/src/lib/plan/plan_pee/mod_prob/problema_pan.py
@@ -1,4 +1,9 @@
 from mod.problema import Problema
 
 
-class ProblemaPan(Problema):pass
+class ProblemaPan(Problema):
+    def __init__(self, modelo_plan, estado_final):
+        self.__estado_final = estado_final
+        
+    def objectivo(self, estado):
+        return super().objectivo(estado)
