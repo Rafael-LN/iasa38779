6e38d13285b1b2a0e96128f2e2ba8df0c9f1bc4d
Rafael-LN <rafaelnunes3092@gmail.com>
2024-05-20 21:40:27 +0100

    Update comments of ModeloPDM interface

--- iasa_agente/src/lib/pdm/modelo/modelo_pdm.py
+++ iasa_agente/src/lib/pdm/modelo/modelo_pdm.py
@@ -1,24 +1,26 @@
 from abc import ABC, abstractmethod
+from mod import Estado, Operador
 
 class ModeloPDM(ABC):
     """
-    A interface ModeloPDM define a estrutura para a modelação de Processos de Decisão de Markov (PDM).
-    
-    Um Processo de Decisão de Markov é utilizado para modelar a tomada de decisão sequencial
-    onde os resultados são parcialmente aleatórios e parcialmente sob o controlo de um agente decisor.
-    
-    Esta interface inclui métodos abstratos para:
-    - Definir o conjunto de estados (S).
-    - Definir o conjunto de ações possíveis (A) em cada estado.
-    - Modelar a transição de estados (T), que define a probabilidade de transição entre estados após uma ação.
-    - Modelar a recompensa (R) associada a uma transição de estado.
-    - Determinar os estados sucessores (suc) possíveis após a execução de uma ação.
+    Interface abstrata representante do modelo de Processos de Decisão de Markov (PDM).
+
+    Esta interface define a estrutura para a modelação de um Processo de Decisão de Markov,
+    onde a tomada de decisão sequencial é influenciada por resultados parcialmente aleatórios 
+    e parcialmente sob o controlo de um agente decisor.
+
+    O PDM é utilizado para representar:
+    - O conjunto de estados possíveis do mundo (S).
+    - O conjunto de ações que o agente pode executar em cada estado (A).
+    - A probabilidade de transição entre estados após a execução de uma ação (T).
+    - A recompensa obtida em função da transição entre estados (R).
+    - Os estados sucessores possíveis após a execução de uma ação (suc).
     """
 
     @abstractmethod
     def S(self):
         """
-        Retorna o conjunto de estados possíveis (S) do mundo.
+        Método abstrato que representa o conjunto de estados do mundo.
         
         :return: Conjunto de estados possíveis.
         """
@@ -27,44 +29,43 @@ class ModeloPDM(ABC):
     @abstractmethod
     def A(self, s):
         """
-        Retorna o conjunto de ações possíveis (A) num estado específico (s).
-        
-        :param s: Estado atual.
+        Método abstrato que representa o conjunto de ações possíveis num estado específico.
+
+        :param s: Estado atual pertencente ao conjunto de estados em S.
         :return: Conjunto de ações possíveis no estado s.
         """
         pass
 
     @abstractmethod
-    def T(self, s, a, sn):
+    def T(self, s, a):
         """
-        Define o modelo de transição de estados (T), ou seja, a probabilidade de transição do estado s para o estado sn através da ação a.
-        
-        :param s: Estado atual.
-        :param a: Ação realizada.
-        :param sn: Estado subsequente.
-        :return: Probabilidade de transição de s para sn através de a.
+        Método abstrato que representa a probabilidade de transição do estado atual para um estado seguinte através de uma ação.
+
+        :param s: Estado atual pertencente ao conjunto de estados em S.
+        :param a: Ação a ser aplicada ao estado s.
+        :return: Lista de tuplos contendo a probabilidade de transição e o estado seguinte.
         """
         pass
 
     @abstractmethod
     def R(self, s, a, sn):
         """
-        Define o modelo de recompensa (R), que representa o ganho ou perda esperado na transição de s para sn através de a.
-        
-        :param s: Estado atual.
-        :param a: Ação realizada.
-        :param sn: Estado subsequente.
-        :return: Recompensa esperada na transição de s para sn através de a.
+        Método abstrato que representa a recompensa esperada pela transição de um estado para outro através de uma ação.
+
+        :param s: Estado atual pertencente ao conjunto de estados em S.
+        :param a: Ação aplicada ao estado s.
+        :param sn: Estado sucessor após a aplicação da ação a no estado s.
+        :return: Valor da recompensa (float).
         """
         pass
 
     @abstractmethod
     def suc(self, s, a):
         """
-        Retorna os estados sucessores possíveis (s') ao aplicar a ação a no estado s.
-        
-        :param s: Estado atual.
-        :param a: Ação realizada.
-        :return: Conjunto de estados sucessores possíveis após aplicar a ação a em s.
+        Método abstrato que retorna os estados sucessores possíveis após a execução de uma ação num estado.
+
+        :param s: Estado atual pertencente ao conjunto de estados em S.
+        :param a: Ação aplicada ao estado s.
+        :return: Conjunto de estados sucessores possíveis.
         """
         pass
