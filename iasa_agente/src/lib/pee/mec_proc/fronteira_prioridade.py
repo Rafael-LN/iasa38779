2c322f83a5edf73418c17802ef03a2a383cdd67d
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:42:05 +0100

    Update FronteiraPrioridade comments

--- iasa_agente/src/lib/pee/mec_proc/fronteira_prioridade.py
+++ iasa_agente/src/lib/pee/mec_proc/fronteira_prioridade.py
@@ -3,42 +3,46 @@ from pee.mec_proc.fronteira import Fronteira
 
 class FronteiraPrioridade(Fronteira):
     """
-    Classe que define uma fronteira de prioridade para armazenar nós de busca, onde o critério de prioridade é determinado por um avaliador.
-
-    Atributos:
-        __avaliador: O avaliador utilizado para determinar a prioridade dos nós.
-
-    Métodos:
-        inserir: Insere um nó na fronteira de acordo com sua prioridade.
-        remover: Remove e retorna o nó com a maior prioridade da fronteira.
+    Classe que implementa uma fronteira de prioridade para algoritmos de procura.
+    Herda da classe Fronteira e utiliza uma heap (fila de prioridade) para ordenar os nós com base na sua prioridade.
     """
 
     def __init__(self, avaliador):
         """
-        Inicializa a fronteira de prioridade com o avaliador especificado.
+        Inicializa uma nova instância de FronteiraPrioridade.
 
-        Args:
-            avaliador: O avaliador utilizado para determinar a prioridade dos nós.
+        Parâmetros:
+        avaliador: O avaliador utilizado para calcular a prioridade dos nós.
+
+        Funcionalidade:
+        Este construtor chama o construtor da classe base Fronteira e inicializa o atributo __avaliador com o avaliador fornecido.
         """
         super().__init__()
         self.__avaliador = avaliador
-        
+
     def inserir(self, no):
         """
-        Insere um nó na fronteira de acordo com sua prioridade.
+        Insere um nó na fronteira de exploração utilizando a prioridade calculada pelo avaliador.
 
-        Args:
-            no: O nó a ser inserido na fronteira.
+        Parâmetros:
+        no: O nó a ser inserido na fronteira.
+
+        Funcionalidade:
+        Este método calcula a prioridade do nó utilizando o avaliador e insere o nó na heap, mantendo a ordem de prioridade.
+        A heap garante que o nó com a menor prioridade será removido primeiro.
         """
         prioridade = self.__avaliador.prioridade(no)
-        heappush(self._nos,(prioridade,no))
-        
+        heappush(self._nos, (prioridade, no))
+
     def remover(self):
         """
-        Remove e retorna o nó com a maior prioridade da fronteira.
+        Remove e retorna o nó com a menor prioridade da fronteira de exploração.
 
-        Retorno:
-            O nó com a maior prioridade na fronteira.
+        Retorna:
+        O nó com a menor prioridade.
+
+        Funcionalidade:
+        Este método remove e retorna o nó com a menor prioridade da heap, garantindo que a exploração siga a ordem de prioridade.
         """
         (_, no) = heappop(self._nos)
         return no
