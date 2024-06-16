40784cf4870d59c3655c715f15ce96c0dcdb5c2e
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:30:38 +0100

    Update ProcuraCustoUnif comments

--- iasa_agente/src/lib/pee/melhor_prim/procura_custo_unif.py
+++ iasa_agente/src/lib/pee/melhor_prim/procura_custo_unif.py
@@ -4,13 +4,15 @@ from pee.melhor_prim.procura_melhor_prim import ProcuraMelhorPrim
 class ProcuraCustoUnif(ProcuraMelhorPrim):
     """
     Classe que implementa o algoritmo de procura de custo uniforme.
-
-    Métodos:
-        __init__: Inicializa a procura de custo uniforme com um avaliador de custo uniforme.
+    Herda da classe ProcuraMelhorPrim e utiliza um avaliador específico que considera o custo uniforme.
     """
 
     def __init__(self):
         """
-        Inicializa a procura de custo uniforme com um avaliador de custo uniforme.
+        Inicializa uma nova instância de ProcuraCustoUnif.
+
+        Funcionalidade:
+        Este construtor chama o construtor da classe base ProcuraMelhorPrim, passando uma instância de AvaliadorCustoUnif.
+        O AvaliadorCustoUnif é utilizado para avaliar os nós com base no custo acumulado do percurso, implementando a estratégia de procura de custo uniforme.
         """
         super().__init__(AvaliadorCustoUnif())
