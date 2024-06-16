be05f9dfdfd13f70d26bebbb8330d4a5de481689
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:59:17 +0100

    Update ProcuraSofrega comments

--- iasa_agente/src/lib/pee/melhor_prim/procura_sofrega.py
+++ iasa_agente/src/lib/pee/melhor_prim/procura_sofrega.py
@@ -1,22 +1,18 @@
 from pee.melhor_prim.aval.avaliador_sof import AvaliadorSof
 from pee.melhor_prim.procura_informada import ProcuraInformada
 
-
 class ProcuraSofrega(ProcuraInformada):
     """
-    Classe que implementa o algoritmo de procura sofrega.
-
-    Herda de ProcuraInformada.
-
-    Atributos:
-        Nenhum atributo adicional além dos herdados de ProcuraInformada.
-
-    Métodos:
-        __init__: Inicializa a classe, especificando o avaliador a ser usado.
+    Classe que implementa o algoritmo de procura Sôfrega (Greedy Search).
+    Herda da classe ProcuraInformada e utiliza o AvaliadorSof para determinar a prioridade dos nós com base na heurística.
     """
 
     def __init__(self):
         """
-        Inicializa a procura sofrega com o AvaliadorSof.
+        Inicializa uma nova instância de ProcuraSofrega.
+
+        Funcionalidade:
+        Este construtor chama o construtor da classe base ProcuraInformada, passando AvaliadorSof como o avaliador
+        que será utilizado para calcular a prioridade dos nós durante a exploração.
         """
-        super().__init__(AvaliadorSof)
+        super().__init__(AvaliadorSof())
