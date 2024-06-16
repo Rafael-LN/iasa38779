4113d3a618fe12979a2f6bd83883181327ae8f22
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:25:55 +0100

    Update ModeloPlan comments

--- iasa_agente/src/lib/plan/modelo/modelo_plan.py
+++ iasa_agente/src/lib/plan/modelo/modelo_plan.py
@@ -2,40 +2,48 @@ from abc import ABC, abstractmethod
 
 class ModeloPlan(ABC):
     """
-    Interface abstrata para representar um modelo de planeamento no contexto de um agente de planeamento.
-
-    Métodos abstratos:
-        obter_estado(self): Retorna o estado atual do modelo de planeamento.
-        obter_estados(self): Retorna todos os estados válidos no modelo de planeamento.
-        obter_operadores(self): Retorna todos os operadores válidos no modelo de planeamento.
+    Classe abstrata que define a interface para um modelo de planeamento.
+    Este modelo é utilizado pelo planeador para obter o estado inicial, os estados válidos e os operadores disponíveis no domínio do problema.
     """
-    
+
     @abstractmethod
     def obter_estado(self):
         """
-        Método abstrato que deve ser implementado para retornar o estado atual do modelo de planeamento.
+        Método abstrato para obter o estado inicial do modelo de planeamento.
 
         Retorna:
-            Estado atual do modelo de planeamento.
+        O estado inicial do modelo de planeamento.
+
+        Funcionalidade:
+        Este método deve ser implementado pelas subclasses para fornecer o estado inicial a partir do qual o planeamento começará.
+        É essencial para definir o ponto de partida no espaço de estados.
         """
         pass
-    
+
     @abstractmethod
     def obter_estados(self):
         """
-        Método abstrato que deve ser implementado para retornar todos os estados válidos no modelo de planeamento.
+        Método abstrato para obter todos os estados válidos do modelo de planeamento.
 
         Retorna:
-            list: Lista de estados válidos.
+        Uma lista de estados válidos no modelo de planeamento.
+
+        Funcionalidade:
+        Este método deve ser implementado pelas subclasses para fornecer todos os estados possíveis no domínio do problema.
+        É necessário para processos que necessitam conhecer todos os estados válidos, como processos de decisão de Markov.
         """
         pass
-    
+
     @abstractmethod
     def obter_operadores(self):
         """
-        Método abstrato que deve ser implementado para retornar todos os operadores válidos no modelo de planeamento.
+        Método abstrato para obter os operadores disponíveis no modelo de planeamento.
 
         Retorna:
-            list: Lista de operadores válidos.
+        Uma lista de operadores que podem ser aplicados aos estados no modelo de planeamento.
+
+        Funcionalidade:
+        Este método deve ser implementado pelas subclasses para fornecer os operadores (ações) que podem transformar um estado em outro.
+        É crucial para definir as transições possíveis no espaço de estados.
         """
         pass
