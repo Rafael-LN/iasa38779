be7c7f7b7ec2ee271c3750762703c6a0a567e62e
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 20:01:00 +0100

    Update ProcuraAA comments

--- iasa_agente/src/lib/pee/melhor_prim/procura_aa.py
+++ iasa_agente/src/lib/pee/melhor_prim/procura_aa.py
@@ -3,14 +3,17 @@ from pee.melhor_prim.procura_informada import ProcuraInformada
 
 class ProcuraAA(ProcuraInformada):
     """
-    Classe que implementa o algoritmo de procura A*.
-
-    Métodos:
-        __init__: Inicializa a procura A* com um avaliador A*.
+    Classe que implementa o algoritmo de procura A* (A Estrela).
+    Herda da classe ProcuraInformada e utiliza o AvaliadorAA para determinar a prioridade dos nós com base na função de avaliação f(n) = g(n) + h(n).
     """
 
     def __init__(self):
         """
-        Inicializa a procura A* com um avaliador A*.
+        Inicializa uma nova instância de ProcuraAA.
+
+        Funcionalidade:
+        Este construtor chama o construtor da classe base ProcuraInformada, passando AvaliadorAA como o avaliador
+        que será utilizado para calcular a prioridade dos nós durante a exploração.
+        O AvaliadorAA combina o custo acumulado (g(n)) e a estimativa heurística (h(n)) para calcular a prioridade.
         """
         super().__init__(AvaliadorAA())
