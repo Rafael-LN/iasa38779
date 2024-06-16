1e3aed1d4b01a7fb8110495a6df7814b81a5a3e5
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:39:45 +0100

    Update FronteiraLIFO comments

--- iasa_agente/src/lib/pee/prof/fronteira_lifo.py
+++ iasa_agente/src/lib/pee/prof/fronteira_lifo.py
@@ -2,24 +2,19 @@ from pee.mec_proc.fronteira import Fronteira
 
 class FronteiraLIFO(Fronteira):
     """
-    A classe FronteiraLIFO implementa uma fronteira para a procura em espaço de estados utilizando uma política de Last-In-First-Out (LIFO).
-
-    Esta estrutura de dados é usada para armazenar e recuperar nós durante o processo de procura, garantindo que o nó mais recentemente inserido é o primeiro a ser retirado.
-
-    Herda:
-        Fronteira: Classe base que define a interface para diferentes tipos de fronteiras.
-
-    Métodos:
-        inserir(self, no): Insere um nó na fronteira utilizando a política LIFO.
+    Classe que implementa uma fronteira LIFO (Last In, First Out) para algoritmos de procura.
+    Herda da classe Fronteira e utiliza uma lista para armazenar os nós, onde o último nó inserido é o primeiro a ser removido.
     """
 
     def inserir(self, no):
         """
-        Insere um nó na fronteira utilizando a política LIFO.
+        Insere um nó na fronteira de exploração utilizando a estratégia LIFO.
 
         Parâmetros:
-            no: O nó a ser inserido na fronteira.
+        no: O nó a ser inserido na fronteira.
 
-        Este método adiciona o nó ao final da lista interna de nós (_nos), garantindo que será o primeiro a ser retirado quando necessário.
+        Funcionalidade:
+        Este método insere o nó no início da lista de nós, garantindo que o nó mais recentemente adicionado será o primeiro a ser removido durante a exploração.
+        A estratégia LIFO é típica de algoritmos de procura em profundidade.
         """
         self._nos.insert(0, no)
