4795c253bee2eee1a8edc36bd76b3f6422663cef
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:52:56 +0100

    Update AvaliadorSof comments

--- iasa_agente/src/lib/pee/melhor_prim/aval/avaliador_sof.py
+++ iasa_agente/src/lib/pee/melhor_prim/aval/avaliador_sof.py
@@ -2,27 +2,23 @@ from pee.melhor_prim.aval.avaliador_heur import AvaliadorHeur
 
 class AvaliadorSof(AvaliadorHeur):
     """
-    A classe AvaliadorSof implementa um avaliador heurístico para algoritmos de procura gulosa (Greedy Search).
-
-    A avaliação é baseada no custo do nó atual, priorizando nós com menor custo, típico em algoritmos de procura que utilizam estratégias ávidas para otimização.
-
-    Herda:
-        AvaliadorHeur: Classe base que define a interface e funcionalidade básica para avaliadores heurísticos.
-
-    Métodos:
-        prioridade(self, no): Retorna a prioridade de um nó com base no seu custo.
+    Classe que implementa um avaliador para o algoritmo de procura Sôfrega (Greedy Search).
+    Herda da classe AvaliadorHeur e utiliza uma heurística para calcular a prioridade dos nós.
     """
 
     def prioridade(self, no):
         """
-        Calcula a prioridade de um nó com base no seu custo.
-
-        Este método é utilizado em algoritmos de procura gulosa onde a decisão de expansão do nó é baseada no custo atual do nó, favorecendo nós com menor custo.
+        Calcula a prioridade de um nó com base na heurística.
 
         Parâmetros:
-            no: O nó cuja prioridade será calculada.
+        no: O nó cuja prioridade será calculada.
 
         Retorna:
-            float: O custo do nó, utilizado como prioridade na expansão.
+        O valor heurístico do estado do nó.
+
+        Funcionalidade:
+        Este método retorna o valor heurístico do estado do nó, calculado pela função heurística.
+        A prioridade é determinada exclusivamente pela estimativa heurística do custo para atingir o objetivo,
+        típica do algoritmo de procura Sôfrega, que sempre expande o nó com a menor estimativa heurística.
         """
         return self._heuristica.h(no.estado)
