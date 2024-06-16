33026c2c102d13eb46f3f4c0050027646ddf331b
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:46:07 +0100

    Update ProcuraMelhorPrim comments

--- iasa_agente/src/lib/pee/melhor_prim/procura_melhor_prim.py
+++ iasa_agente/src/lib/pee/melhor_prim/procura_melhor_prim.py
@@ -3,35 +3,37 @@ from pee.mec_proc.fronteira_prioridade import FronteiraPrioridade
 
 class ProcuraMelhorPrim(ProcuraGrafo):
     """
-    Classe que define o comportamento de uma procura utilizando a estratégia de Melhor-Prim.
-
-    Herda de ProcuraGrafo.
-
-    Métodos:
-        __init__: Método de inicialização que define o avaliador a ser utilizado na procura.
-        _manter: Método para determinar se um nó deve ser mantido na memória durante a procura.
+    Classe que implementa o algoritmo de procura melhor-primeiro.
+    Herda da classe ProcuraGrafo e utiliza uma fronteira de prioridade para a exploração dos nós, onde a prioridade é determinada por um avaliador.
     """
 
     def __init__(self, avaliador):
         """
-        Inicializa a procura com o avaliador especificado.
+        Inicializa uma nova instância de ProcuraMelhorPrim.
 
-        Args:
-            avaliador: O avaliador a ser utilizado na procura.
+        Parâmetros:
+        avaliador: O avaliador utilizado para calcular a prioridade dos nós.
+
+        Funcionalidade:
+        Este construtor chama o construtor da classe base ProcuraGrafo com uma fronteira de prioridade,
+        utilizando o avaliador fornecido para determinar a prioridade dos nós.
+        Também inicializa o atributo _avaliador com o avaliador fornecido.
         """
         super().__init__(FronteiraPrioridade(avaliador))
         self._avaliador = avaliador
-        
+
     def _manter(self, no):
         """
-        Determina se um nó deve ser mantido na memória durante a procura.
+        Determina se um nó deve ser mantido na fronteira de exploração.
 
-        Este método é abstrato e deve ser implementado pelas subclasses.
+        Parâmetros:
+        no: O nó a ser avaliado.
 
-        Args:
-            no: O nó a ser verificado.
+        Retorna:
+        True se o nó deve ser mantido, False caso contrário.
 
-        Returns:
-            True se o nó deve ser mantido, False caso contrário.
+        Funcionalidade:
+        Este método verifica se o nó deve ser mantido na fronteira de exploração.
+        Um nó é mantido se ele ainda não foi explorado ou se o custo atual do nó é menor do que o custo registrado para o mesmo estado.
         """
         return super()._manter(no) or no.custo < self._explorados.get(no.estado).custo
