6596074055d2791292302777cd0e2452d0edf4e8
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-16 19:41:08 +0100

    Update ProcuraProfIter comments

--- iasa_agente/src/lib/pee/prof/procura_prof_Iter.py
+++ iasa_agente/src/lib/pee/prof/procura_prof_Iter.py
@@ -2,30 +2,26 @@ from pee.prof.procura_prof_lim import ProcuraProfLim
 
 class ProcuraProfIter(ProcuraProfLim):
     """
-    A classe ProcuraProfIter implementa a procura em profundidade iterativa, estendendo a classe ProcuraProfLim.
-
-    A procura em profundidade iterativa é uma técnica que combina a profundidade limitada e a profundidade iterativa, aumentando gradualmente o limite de profundidade até encontrar uma solução ou atingir o limite máximo.
-
-    Herda:
-        ProcuraProfLim: Classe base que implementa a procura em profundidade com um limite máximo de profundidade.
-
-    Métodos:
-        procurar(self, problema, inc_prof, limite_prof): Realiza a procura em profundidade iterativa para resolver o problema fornecido.
+    Classe que implementa o algoritmo de procura em profundidade iterativa.
+    Herda da classe ProcuraProfLim e realiza múltiplas buscas em profundidade limitada com profundidade crescente.
     """
 
     def procurar(self, problema, inc_prof, limite_prof):
         """
-        Realiza a procura em profundidade iterativa para resolver o problema fornecido.
-
-        Este método incrementa gradualmente o limite de profundidade, realizando a procura em cada nível até encontrar uma solução ou atingir o limite máximo.
+        Realiza a procura de uma solução utilizando a estratégia de profundidade iterativa.
 
         Parâmetros:
-            problema: O problema a ser resolvido, definido como um espaço de estados com operadores e objetivos.
-            inc_prof (int): O incremento da profundidade a cada iteração.
-            limite_prof (int): O limite máximo de profundidade para a procura.
+        problema: O problema de planeamento a ser resolvido.
+        inc_prof: O incremento de profundidade a ser utilizado em cada iteração.
+        limite_prof: A profundidade máxima a ser alcançada nas iterações.
 
         Retorna:
-            solucao: A solução encontrada para o problema, ou None se nenhuma solução for encontrada dentro do limite de profundidade.
+        A solução encontrada se houver, ou None se não for encontrada nenhuma solução dentro do limite de profundidade.
+
+        Funcionalidade:
+        Este método realiza buscas em profundidade limitada com profundidade crescente, começando de zero até o limite especificado.
+        Em cada iteração, a profundidade máxima é aumentada pelo incremento especificado e a busca é realizada.
+        Se uma solução for encontrada, é retornada imediatamente; caso contrário, a busca continua até atingir o limite de profundidade.
         """
         self.prof_max = 0  # Inicializa a profundidade máxima no método da classe base
         for i in range(0, limite_prof, inc_prof):
